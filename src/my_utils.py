75fbe73ef70fc8ecc94f80dc4a8763ec8cdb56dc
Shinnnyshinshin <willshin@gmail.com>
2019-10-20 10:18:10 -0400

    Update my_utils.py

--- src/my_utils.py
+++ src/my_utils.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-import math
+#import math
 
 # this is for the strange way that python 3.7 does the rounding
 def myround(numtoRound):
-    if numtoRound - math.floor(numtoRound >= 0.5):
-        return int(math.ceil(numtoRound) + 10000)
+    if numtoRound - int(numtoRound) >= 0.5:
+        return int(numtoRound) + 1
     else:
-        return int(math.floor(numtoRound) + 10000)
+        return int(numtoRound)
